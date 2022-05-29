2515707ab3f547da0074306db9d189efbc5062df
pila89 <salim.amara@outlook.com>
2022-05-29 02:45:16 +0200

    toulou

--- server/src/controllers/images.controllers.js
+++ server/src/controllers/images.controllers.js
@@ -1,18 +1,22 @@
 const Image = require("@models/image");
 const upload = require("../uti/upload");
 const uploadImage = upload.single("image");
+const path = require("path");
+const fs = require("fs");
+
 
 exports.addImage = async (req, res) => {
   uploadImage(req, res, async function (err) {
+    console.log();
     if (err) {
       res.status(404).json({ image: err.message });
     } else {
       const image = {
         title: req.body.title,
         image: req.file.filename,
-        path: process.env.BASE_URL + "/images" + req.file.filename,
+        path: process.env.BASE_URL + "/images/" + req.file.filename,
       };
-      await Image.create(image);
+      //await Image.create(image);
       await Image.find().then((result) => {
         res.status(200).json(result);
       });
@@ -21,23 +25,24 @@ exports.addImage = async (req, res) => {
   const newImage = new Image(req.body);
   await newImage.save();
   res.status(200).send({ newImage, msg: "image uploaded" });
+};
 
-}; 
+exports.findImage = async (req, res) => {
+  try {
+    let result = await Image.find();
 
-// exports.findImage=async (req, res) =>
-//  {try {
-//       let result = await Image.findByIdAndRemove({ _id: req.params.id });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(200).json(result);
+  }
+};
 
-//      res.send( "image found");
-//     } catch (error) {
-//    console.log(error);
-//      }};
+ exports.deleteImage=async (req, res) =>{
 
-//  exports.deleteImage=async (req, res) =>
-//  {try {
-//        let result = await Image.findByIdAndRemove({ _id: req.params.id });
+  try {
+    let result = await Image.findByIdAndRemove({ _id: req.params.id });
 
-//       res.send( "image deleted");
-//      } catch (error) {
-//     console.log(error);
-//       }};
+    res.send("image removed");
+  } catch (error) {
+    console.log(error);
+  }};
